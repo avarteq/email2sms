6acde5d440956fa88c9560a430c51fd521559a5b
Julian Fischer <fischer@avarteq.de>
2009-08-27 10:24:25 +0200

    bugfix while starting the daemon without config files present

--- lib/config.rb
+++ lib/config.rb
@@ -12,27 +12,37 @@ module Config
     
     # Check or create config file
     unless File.exists?(config_file_path) then
+      puts "\n\n-------------------------------------\n"
+      puts "Didn't find a configuration file in #{config_file_path}. First start?"
+      puts "I am going to create a configuration file template for you under #{config_file_path}."
+      puts "Please adapt it to your needs and start the daemon again."
+      puts "-------------------------------------\n\n"
       config_file = File.new(config_file_path, "w+")
       config_file.puts File.open(default_config_file_path).read
     end
 
     config = YAML.load_file(config_file_path)
-    config  
+    config 
   end
   
   def self.logdir
     mylogdir = ""
+    
     config = self.load
-        
-    if config["log"]["dir_mode"] == "current" then
-      mylogdir = Dir.pwd
-    elsif config["log"]["dir_mode"] == "normal" then
-      mylogdir = config["log"]["dir"]      
-      exists_or_create_dir(mylogdir)
-    else
-      raise "Unknown log dir_mode #{config[:log][:dir_mode]}. Possible values are: 'current' and 'normal'."
-    end  
-    mylogdir
+    
+    # If there is no configuration file, it will be generated but it wouldn't make 
+    # sense to load it. The user has to customize it, first.
+    if config    
+      if config["log"]["dir_mode"] == "current" then
+        mylogdir = Dir.pwd
+      elsif config["log"]["dir_mode"] == "normal" then
+        mylogdir = config["log"]["dir"]      
+        exists_or_create_dir(mylogdir)
+      else
+        raise "Unknown log dir_mode #{config[:log][:dir_mode]}. Possible values are: 'current' and 'normal'."
+      end  
+    end
+    mylogdir || Dir.pwd
   end
   
   protected
